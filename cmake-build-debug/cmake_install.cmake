25fd8c532c695d315c94c4590204bb505c33852f
damir <damir14012008@mail.ru>
2024-10-23 12:29:09 +0300

    add file

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/damir/CLionProjects/LangugePrigram
+# Install script for directory: /Users/damir/CLionProjects/programming-language_main
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/damir/CLionProjects/LangugePrigram/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/damir/CLionProjects/programming-language_main/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
